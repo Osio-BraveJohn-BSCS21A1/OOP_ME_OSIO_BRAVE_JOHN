fab2411b3a923ade795b397268c9791f1bfd74ee
Osio-BraveJohn-BSCS21A1 <c2059-23@itmlyceumalabang.onmicrosoft.com>
2024-10-12 12:49:02 +0800

    Add files via upload

---
+++ ConsoleApp1/Workspace/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1.Exam
+{
+    public class Car
+    {
+        private readonly Engine _engine;
+
+        public bool IsEngineStarted { get; set; } = true;
+
+        public Car(Engine engine)
+        {
+            _engine = engine;
+        }
+
+        public void StartCar()
+        {
+            _engine.Start();
+        }
+
+    }
+
+    // Car and Engine (Composition example)
+    public class Engine
+    {
+        public void Start()
+        {
+            
+        }
+    }
+
+}
+
+
